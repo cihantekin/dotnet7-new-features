bf1fb45656b50133b71481f750e68b46c2f2fc23
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-10-28 18:45:36 +0200

    Modifying JSON Contracts

---
+++ dotnet7-new-features/UpperCasePropertyContractResolver.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace dotnet7_new_features
+{
+    public class UpperCasePropertyContractResolver : DefaultJsonTypeInfoResolver
+    {
+        public override JsonTypeInfo GetTypeInfo(Type type, JsonSerializerOptions options)
+        {
+            JsonTypeInfo info = base.GetTypeInfo(type, options);
+
+            if (info.Kind == JsonTypeInfoKind.Object)
+            {
+                foreach (var prop in info.Properties)
+                {
+                    prop.Name = prop.Name.ToUpperInvariant();
+                }
+            }
+
+            return info;
+        }
+
+        public string SerializeMyModel()
+        {
+            var options = new JsonSerializerOptions { TypeInfoResolver = new UpperCasePropertyContractResolver() };
+            return JsonSerializer.Serialize(new { value = "uppercase test" }, options);
+        }
+    }
+}
