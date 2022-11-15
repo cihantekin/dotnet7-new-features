14c9190492ac7530f768e288855ac579a3a5c817
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-11-15 17:27:42 +0100

    Endpoint filters for minimal apis

---
+++ dotnet7-new-features/EndpointFilters/ValidationFilter.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System.Net;
+
+namespace dotnet7_new_features.EndpointFilters
+{
+    public class ValidationFilter<T> : IEndpointFilter
+    {
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+        {
+            T? argumentToValidate = context.GetArgument<T>(0);
+            IValidator<T>? validator = context.HttpContext.RequestServices.GetServices<IValidator<T>>().FirstOrDefault();
+
+            if (validator is not null)
+            {
+                var validationResult = await validator.ValidateAsync(argumentToValidate);
+                if (!validationResult.IsValid)
+                {
+                    return Results.ValidationProblem(validationResult.ToDictionary(), statusCode: (int)HttpStatusCode.UnprocessableEntity);
+
+                }
+            }
+
+            return await next.Invoke(context);
+        }
+    }
+}
