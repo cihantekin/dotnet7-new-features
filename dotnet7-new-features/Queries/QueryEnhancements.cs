eb11fb9b36e4dd9c5009d86e44ca5b86586be608
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-11-24 22:56:02 +0100

    EF Core 7 included

---
+++ dotnet7-new-features/Queries/QueryEnhancements.cs
@@ -0,0 +1,18 @@
+﻿namespace dotnet7_new_features.Queries
+{
+    public class QueryEnhancements
+    {
+        public QueryEnhancements()
+        {
+        using var ctx = new QueryEnhancementsContext();
+
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ dotnet7-new-features/Queries/QueryEnhancementsContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dotnet7_new_features.Queries
+{
+    internal class QueryEnhancementsContext : DbContext
+    {
+        public QueryEnhancementsContext()
+        {
+        }
+    }
+}
