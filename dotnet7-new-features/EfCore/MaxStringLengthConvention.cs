795fc6cbdb73aa58124adf2be1cb30efc106441d
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-11-28 19:22:59 +0100

    MaxStringLengthConvention

---
+++ dotnet7-new-features/EfCore/MaxStringLengthConvention.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace dotnet7_new_features.EfCore
+{
+    public class MaxStringLengthConvention : IModelFinalizingConvention
+    {
+        public void ProcessModelFinalizing(IConventionModelBuilder modelBuilder, IConventionContext<IConventionModelBuilder> context)
+        {
+            foreach (var property in modelBuilder.Metadata.GetEntityTypes().SelectMany(e => e.GetDeclaredProperties().Where(prop => prop.ClrType == typeof(string))))
+            {
+                property.Builder.HasMaxLength(512);
+            }
+        }
+    }
+}
