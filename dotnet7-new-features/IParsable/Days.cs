8843e52207c297c05a325b3f4c4ed268366b6f4d
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-12-22 00:16:06 +0100

    IParsable to create an object from a string

---
+++ dotnet7-new-features/IParsable/Days.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace dotnet7_new_features.IParseable
+{
+    // /WeatherForecast/1-3 to be able to use like that
+    // this is like a model binder but a new way to do it.
+    public readonly struct Days : IParsable<Days>
+    {
+        public int From { get; }
+        public int To { get; }
+
+        public Days(int from, int to)
+        {
+            From = from;
+            To = to;
+        }
+      
+        public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Days result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Days Parse(string s, IFormatProvider? provider)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
