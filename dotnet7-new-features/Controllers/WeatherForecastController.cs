e4292597885112ba5fbead862db3e2a799c5f324
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-12-23 00:30:59 +0100

    endpoint for parsable

--- dotnet7-new-features/Controllers/WeatherForecastController.cs
+++ dotnet7-new-features/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using dotnet7_new_features.IParseable;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,6 +24,19 @@ namespace dotnet7_new_features.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        //call this endpoint like: WeatherForecast/1-4
+        [HttpGet("{days}", Name = "GetWithParsable")]
+        public IEnumerable<WeatherForecast> GetWithParsable(Days days)
+        {
+            return Enumerable.Range(days.From, days.To).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
