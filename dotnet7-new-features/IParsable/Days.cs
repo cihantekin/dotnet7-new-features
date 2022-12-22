0f5d792c3f81ad92579b94f9fbdcafeb6cdbcb07
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-12-23 00:15:04 +0100

    IParsable implementation

--- dotnet7-new-features/IParsable/Days.cs
+++ dotnet7-new-features/IParsable/Days.cs
@@ -4,6 +4,7 @@ namespace dotnet7_new_features.IParseable
 {
     // /WeatherForecast/1-3 to be able to use like that
     // this is like a model binder but a new way to do it.
+    // any string coming to an endpoint could be converted as an object just implementing this TryParse method
     public readonly struct Days : IParsable<Days>
     {
         public int From { get; }
@@ -17,12 +18,34 @@ namespace dotnet7_new_features.IParseable
       
         public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Days result)
         {
-            throw new NotImplementedException();
+            if (s is not null)
+            {
+                var separator = s.IndexOf("-");
+                if (separator > 0 && separator < s.Length - 1)
+                {
+                    var fromSpan = s.AsSpan().Slice(0, separator);
+                    var toSpan = s.AsSpan(separator + 1);
+
+                    if (int.TryParse(fromSpan, System.Globalization.NumberStyles.None, provider, out var from) && int.TryParse(toSpan, System.Globalization.NumberStyles.None, provider, out var to))
+                    {
+                        result = new Days(from, to);
+                        return true;
+                    }
+                }
+            }
+
+            result = default;
+            return false;
         }
 
         public static Days Parse(string s, IFormatProvider? provider)
         {
-            throw new NotImplementedException();
+            if (!TryParse(s, provider, out var result))
+            {
+                throw new ArgumentException("Could not parse supplied value.", nameof(s));
+            }
+
+            return result;
         }
 
     }
