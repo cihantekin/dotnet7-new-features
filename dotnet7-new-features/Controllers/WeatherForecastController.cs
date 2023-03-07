66e45d6d586f10474468a9206c3e5c190c046f09
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-03-07 16:43:21 +0100

    use new visual studio build in http client to test endpoints

--- dotnet7-new-features/Controllers/WeatherForecastController.cs
+++ dotnet7-new-features/Controllers/WeatherForecastController.cs
@@ -12,13 +12,6 @@ namespace dotnet7_new_features.Controllers
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
