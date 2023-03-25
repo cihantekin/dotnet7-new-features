fbd92774bc18471d93d79e817c8a9b183e63b3ed
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-03-25 21:22:54 +0100

    PersonSerializationContext

---
+++ dotnet7-new-features/Json.Text/PersonSerializationContext.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace dotnet7_new_features.Json.Text
+{
+    [JsonSourceGenerationOptions(WriteIndented = true, DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull, PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+    [JsonSerializable(typeof(Person))]
+    public partial class PersonSerializationContext : JsonSerializerContext { }
+}
---
+++ dotnet7-new-features/Model/Person.cs
@@ -0,0 +1,6 @@
+﻿using System.Globalization;
+
+namespace dotnet7_new_features.Model
+{
+    public record Person (string Name, DateTime BirthDate, Person? Friend, bool? IsCitizen = true);
+}
